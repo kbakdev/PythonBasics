f93bd8331469137bbe8b9c3f07f3704f45ed8a11
Sejki <bonczalke@gmail.com>
2020-04-18 13:52:01 +0200

    Task 15-16

---
+++ Task15.py
@@ -0,0 +1,5 @@
+# Napisz program wypisujący liczby do 20 do 1 (w
+# tej kolejności)
+
+for i in range (20, 0, -1):
+    print(i)
---
+++ Task16.py
@@ -0,0 +1,4 @@
+#16. Napisz program drukujący na ekranie 10 gwiazdek:
+
+for i in range (1, 11):
+    print ('*')
